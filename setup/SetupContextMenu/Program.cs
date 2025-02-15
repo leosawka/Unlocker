fd98b68285e5f5206359a93ad5142fcd2d355475
leosawka <56418748+leosawka@users.noreply.github.com>
2025-02-15 01:48:55 -0300

    First commit

---
+++ setup/SetupContextMenu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Win32;
+using System.IO;
+
+class RegisterContextMenu
+{
+    static void Main()
+    {
+        string exePath = @"C:\Program Files\Unlocker\Unlocker.exe";
+
+        if (!File.Exists(exePath))
+        {
+            Console.WriteLine("Error: El archivo Unlocker.exe no se encuentra en la ruta especificada.");
+            return;
+        }
+
+        using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"*\shell\Unlocker"))
+        {
+            key.SetValue("", "Unlocker");
+            key.SetValue("Icon", exePath);
+        }
+
+        using (RegistryKey commandKey = Registry.ClassesRoot.CreateSubKey(@"*\shell\Unlocker\command"))
+        {
+            commandKey.SetValue("", $"\"{exePath}\" \"%1\"");
+        }
+
+        using (RegistryKey dirKey = Registry.ClassesRoot.CreateSubKey(@"Directory\shell\Unlocker"))
+        {
+            dirKey.SetValue("", "Unlocker");
+            dirKey.SetValue("Icon", exePath);
+        }
+
+        using (RegistryKey dirCommandKey = Registry.ClassesRoot.CreateSubKey(@"Directory\shell\Unlocker\command"))
+        {
+            dirCommandKey.SetValue("", $"\"{exePath}\" \"%1\"");
+        }
+
+        Console.WriteLine("Unlocker agregado al menú contextual con éxito.");
+    }
+}
